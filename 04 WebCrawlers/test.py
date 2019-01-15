4e309d1e5f0c2bdadeaf78ac23b3c7440f2bf4a4
Marco Wang <m.aesophor@gmail.com>
2019-01-15 20:47:14 +0800

    Reorganized project directory layout

--- 04 WebCrawlers/test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-import requests
-from bs4 import BeautifulSoup
-
-if __name__ == '__main__':
-    resp = requests.get("https://shopee.tw/search?keyword=macbook")
-    print(resp.text)
